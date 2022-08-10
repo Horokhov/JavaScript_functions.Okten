c23f303b0b785b563d6ff2e0ad515843f8f658a1
MaxHorokhov <maxhorokhov@gmail.com>
2022-08-10 07:08:26 +0300

    js

--- JS_functions2/main.js
+++ JS_functions2/main.js
@@ -98,4 +98,21 @@
 
 let suits = ["spades", "diamonds", "clubs", "hearts"];
 let values = ["6", "7", "8", "9", "10", "J", "Q", "K","A","Joker"];
-let color = ["red", "black"]
+
+function getDeck()
+{
+    let deck = new Array();
+
+    for(let i = 0; i < suits.length; i++)
+    {
+        for(let x = 0; x < values.length; x++)
+        {
+            let card = {Value: values[x], Suit: suits[i]};
+            deck.push(card);
+        }
+    }
+
+    return deck;
+}
+console.log(getDeck());
+
